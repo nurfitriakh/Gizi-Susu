e0ce3e2fedbde01820eaab9cbf9297b48a3d7485
nurfitriakh <nurfitriakh@gmail.com>
2022-10-18 10:11:47 +0700

    edit

--- GBSS.py
+++ GBSS.py
@@ -51,9 +51,10 @@ if option == 'Home' or option == '':
     st.write("Dari berita yang dimuat pada website suara.com tanggal 4 Agustus 2022, 7 juta anak Indonesia alami kekurangan gizi akibat dari adanya pandemi covid-19. Kemenkes mencatat, kasus stunting saat ini masih ada 24,4 persen dari sebelumnya 30,8 persen pada 2018. Sementara target pemerintah pada 2024, angka stunting tersisa 14 persen. ")
     st.write("Salah satu upaya yang dilakukan Kabupaten Subang dalam mengatasi permasalahan gizi adalah dengan program peningkatan konsumsi susu dalam kehidupan sehari-hari. ")
     st.header("Hipotesis")
-    st.write("Diperkirakan akibat kondisi pandemi setelah dua tahun ini, menyebabkan peningkatan jumlah bayi kurus menjadi 15 persen")
+    st.write("Akibat kondisi pandemi setelah dua tahun ini, menyebabkan peningkatan jumlah bayi kurus dan kurang gizi menjadi 15 persen")
     st.header("Metode")
     st.write("Proses pengumpulan data dilakukan dengan cara mengambil dan mengekstraksi data dari sebuah website atau dikenal juga dengan Web Scraping")
+
 elif option == 'Dataframe':
     st.write("""## Dataframe""") #menampilkan judul halaman dataframe
     #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data 
@@ -66,7 +67,8 @@ elif option == 'Dataframe':
     # st.dataframe(dfgbfilters1) #menampilkan dataframe
     
     #filter subang saja
-    st.write("Data Frame Filter Subang")
+    st.header("Data Frame Gizi Buruk di Subang")
+    st.write("Berikut terlampir data ")
     dfgbfilters = dfgiziburuk[dfgiziburuk['bps_nama_kabupaten_kota'] == 'KABUPATEN SUBANG']
     st.dataframe(dfgbfilters) #menampilkan dataframe
 
